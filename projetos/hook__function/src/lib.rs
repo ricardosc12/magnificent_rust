5f798eb006da1c98f6a219d2b2a9079311db9135
Ricardo <ricardo.spinola9@gmail.com>
2022-09-08 23:29:56 -0300

    function hook e inserção de jmp instruction

--- projetos/hook__function/src/lib.rs
+++ projetos/hook__function/src/lib.rs
@@ -7,6 +7,18 @@
 // https://gist.github.com/CoolOppo/67b452c125bb0db3212a9fbc44c84245,
 // https://github.com/rkarp/rust-dll-demo/blob/master/src/lib.rs
 
+//jmp tem 5 bytes
+// E9 FF FF FF FF → E9 jmp instruction
+//o resto deve ser o endereço relativo em ordem do menos significativo para o mais significativo
+// exemplo 0x40147D → 0x401484
+// 0x401484 - 0x40147D = 7 → -5 do jmp = 2
+// #entender porque -5#
+// 2 u32 em array de 4 bytes = [0,0,0,2]
+// porque 4 ? jmp tem 5, -1 dele, sobra 4
+// então fica E9 02 00 00 00 → menos significativo para mais
+
+// Compilar em arquitetura 32bits
+// cargo build --lib --target=i686-pc-windows-msvc
 
 #![cfg(windows)]
 
@@ -53,39 +65,53 @@ fn hook_function() {
         
     consoleapi::AllocConsole();
 
+    // let relatiave_address = unsafe { std::mem::transmute::<_, u32>(&on_check_sn) } ;
+
+    // println!("address: {:?}",relatiave_address);
+
+
     let mut to_hook: DWORD = 0x40147D;
     
     let mut cur_protection: DWORD = 0;
 
     VirtualProtect(to_hook as LPVOID, 7, 0x40, &mut cur_protection);
     
-
     let buf =  slice::from_raw_parts_mut(to_hook as *mut u8, 7);
 
     for x in 0..7 {
         buf[x] = 0x90;
     }
 
-    let relative_addr: DWORD = our_funct as DWORD - to_hook as DWORD - 5;
+
+    let relatiave_address = unsafe { std::mem::transmute::<_, u32>(&our_funct) } ;
+
+    //Calcular endereço relativo função destino - partida - 5
+    let rel = 0x401484 as u32 - to_hook - 5;
+
+    let bytes: [u8; 4] =  transmute(rel.to_be());
 
     buf[0] = 0xE9;
 
-    let bytes: [u8; 4] = [0x53,0xF8,0x10,0x10];
+    for x in 1..5 {
+        buf[x] = bytes[4-x]
+    }
+
+    // let bytes: [u8; 4] = [0x53,0xF8,0x10,0x10];
     // buf[1] = 0xD8;
 
     // let bytes = relative_addr.to_le_bytes();
 
-    for x in 1..5 {
-        buf[x] = bytes[x];
-    }
+    // for x in 1..5 {
+    //     buf[x] = bytes[x];
+    // }
 
-    // buf[1..5].copy_from_slice(0x401484u32.to_le_bytes());
+    // // buf[1..5].copy_from_slice(0x401484u32.to_le_bytes());
 
-	// // Hook((void*)hookAddress, ourFunct, hookLength);
+	// // // Hook((void*)hookAddress, ourFunct, hookLength);
 
-    let mut temp: DWORD = 0;
+    // let mut temp: DWORD = 0;
 
-	VirtualProtect(to_hook as LPVOID, 7, cur_protection, &mut temp);
+	// VirtualProtect(to_hook as LPVOID, 7, cur_protection, &mut temp);
 
 
 

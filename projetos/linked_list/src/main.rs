560f5f10ce11bb6f57daf6cf9215f59b71e6c812
Ricardo <ricardo.spinola9@gmail.com>
2023-09-25 01:00:45 -0300

    delete - linked_list

--- projetos/linked_list/src/main.rs
+++ projetos/linked_list/src/main.rs
@@ -1,25 +1,40 @@
-
 struct LinkedList {
-    value: u32,
-    next: Option<Box<LinkedList>>,
+    node: Option<Node>,
 }
-impl LinkedList {
-    fn create(value: u32) -> LinkedList {
-        LinkedList {
+#[derive(Clone)]
+struct Node {
+    value: u32,
+    next: Option<Box<Node>>,
+}
+impl Node {
+    fn create(value: u32) -> Node {
+        Node {
             value: value,
             next: None,
         }
     }
-
     fn insert(&mut self, value: u32) {
         if self.next.is_none() {
-            self.next = Some(Box::new(LinkedList { value: value, next: None }));
-        }
-        else {
+            self.next = Some(Box::new(Node {
+                value: value,
+                next: None,
+            }));
+        } else {
             self.next.as_mut().unwrap().insert(value);
         }
     }
+    fn delete(&mut self, value: u32) {
+        let mut current = self;
 
+        while current.next.is_some() {
+            if current.next.as_ref().unwrap().value == value {
+                current.next = current.next.clone().unwrap().next;
+            }
+            else {
+                current = current.next.as_mut().unwrap().as_mut();
+            }
+        }        
+    }
     fn print(&self) {
         print!("{} ", self.value);
 
@@ -28,9 +43,56 @@ impl LinkedList {
         }
     }
 }
+impl LinkedList {
+    fn create(value: u32) -> LinkedList {
+        LinkedList {
+            node: Some(Node::create(value)),
+        }
+    }
+    fn insert(&mut self, value: u32) {
+        if self.node.is_none() {
+            self.node = Some(Node {
+                value: value,
+                next: None,
+            })
+        } else {
+            self.node.as_mut().unwrap().insert(value);
+        }
+    }
+    fn delete(&mut self, value: u32) {
+        if self.node.is_none() {
+            println!("Empty list!");
+        } else {
+            if self.node.as_ref().unwrap().value == value {
+                if self.node.as_ref().unwrap().next.is_some() {
+                    self.node = Some(*self.node.as_ref().unwrap().next.as_ref().unwrap().clone());
+                }
+                else {
+                    self.node = None;
+                }
+            }
+            else {
+                self.node.as_mut().unwrap().delete(value);
+            }
+        }
+    }
+    fn print(&self) {
+        if self.node.is_some() {
+            self.node.as_ref().unwrap().print();
+        } else {
+            println!("Empty list!");
+        }
+    }
+}
 
 fn main() {
-    let mut linked_list = LinkedList::create(10);
-    linked_list.insert(20);
+    let mut linked_list = LinkedList::create(1);
+
+    linked_list.insert(2);
+    linked_list.insert(3);
+    linked_list.delete(1);
+    linked_list.delete(2);
+    linked_list.delete(3);
+    
     linked_list.print();
 }
